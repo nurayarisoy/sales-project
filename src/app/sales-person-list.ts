4a58e33a54ebc39fec93fc893a872521a9bdd062
Nuray arisoy <cevriyeceven@gmail.com>
2022-10-14 13:13:46 +0200

    add

---
+++ src/app/sales-person-list.ts
@@ -0,0 +1,12 @@
+export class SalesPersonList {
+    constructor(public firstname: string,
+        public lastname:string,
+        public email:string,
+        public salecVolume:number ){
+            
+        }
+
+
+
+}
+
